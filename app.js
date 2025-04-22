bcdacf0c2a90f9d99eebded7436bbed33394e5e2
Darwin Xu <darwin@icloud.com>
2025-04-22 09:13:47 +0800

    change default port

--- app.js
+++ app.js
@@ -79,5 +79,5 @@ app.post('/delete', async (req, res) => {
 });
 
 // Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
